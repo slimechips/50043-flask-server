4bc8c3b3df13cda8f4b4a8ba6fb560934ad48e4b
Simon525 <41954443+Simon525@users.noreply.github.com>
2020-11-10 01:15:37 +0800

    Update routes.py

--- flask-db/app/routes.py
+++ flask-db/app/routes.py
@@ -3,6 +3,7 @@ from app import app, bcrypt, db
 from forms import RegisterForm
 from models import User
 
+a = 1
 
 @app.route('/')
 def index():
@@ -10,15 +11,17 @@ def index():
 
 @app.route('/register',methods=['GET','POST'])
 def register():
+    global a
     form = RegisterForm()
     if form.validate_on_submit():
         #commit user to db
         username = form.username.data
         email = form.email.data
         password = bcrypt.generate_password_hash(form.password.data)
-        user = User(username=username,email=email,password=password)
+        user = User(id=a,username=username,email=email,password=password)
         db.session.add(user)
         db.session.commit()
         flash('Congrats, registeration success!',category='success')
+        a += 1
         return redirect(url_for('index'))
     return render_template('register.html',form = form)
